b973672dca21c0b042359858b2560f09adb66240
Yen <yenchieh86@hotmail.com>
2016-12-15 04:48:46 +0000

    create models and set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+    resources :places, only: [:show, :create, :edit, :update, :destroy]
+    
+    root to: 'places#index'
+
 end
