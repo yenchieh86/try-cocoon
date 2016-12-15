539341f7032a299fcd3ad352a249ac436e9c365f
Yen <yenchieh86@hotmail.com>
2016-12-15 04:56:44 +0000

    set place controller's actions

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,37 @@
 class PlacesController < ApplicationController
+    
+    def index
+        @places = Place.all
+    end
+    
+    def show
+        @place = Place.find(params[:id])
+    end
+    
+    def new
+        @place = Place.new
+    end
+    
+    def create
+        @place = Place.new(place_params)
+        
+        if @place.save
+            flash[:notice] = "You just saved a favorite place."
+            redirect_to root_path
+        else
+            flash[:alert] = "Somethings wrong, please try again."
+            render :new
+        end
+    end
+    
+    def destroy
+        
+    end
+    
+    private
+    
+    def place_params
+        params.require(:place).permit(:title)
+    end
+    
 end
